f170083192e85b0f5a37dc15929c47b5482da2c9
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-09-30 23:21:30 -0400

    Power regression tests implemented and power regression passing unit tests.

--- tests/RegressionModel/PowerRegressionTest.php
+++ tests/RegressionModel/PowerRegressionTest.php
@@ -6,26 +6,26 @@ class PowerRegressionTest extends CustomPHPUnit {
 	private $regressionModel;
 
 	public function __construct() {
-		$datax = array();
-		$datay = array();
+		$datax = array(17.6, 26, 31.9, 38.9, 45.8, 51.2, 58.1, 64.7, 66.7, 80.8, 82.9);
+		$datay = array(159.9, 206.9, 236.8, 269.9, 300.6, 323.6, 351.7, 377.6, 384.1, 437.2, 444.7);
 
-		//$this->regressionModel = new PowerRegression($datax, $datay);
+		$this->regressionModel = new PowerRegression($datax, $datay);
 	}
 	
 	public function test_predict() {
-		//$this->assertEquals(139.329214591, round($this->regressionModel->predict(50), 10));
+		$this->assertEquals(305.7034150458, round($this->regressionModel->predict(47), 10));
 	}
 	
 	public function test_getAlpha() {
-		//$this->assertEquals(91.0043962074, round($this->regressionModel->getAlpha(), 10));
+		$this->assertEquals(24.12989312, round($this->regressionModel->getAlpha(), 8));
 	}
 	
 	public function test_getBeta() {
-		//$this->assertEquals(0.9664963677, round($this->regressionModel->getBeta(), 10));
+		$this->assertEquals(0.65949782, round($this->regressionModel->getBeta(), 8));
 	}
 	
 	public function test_getCorrelation() {
-		//$this->assertEquals(0.9341638423, round($this->regressionModel->getCorrelation(), 10));
+		$this->assertEquals(0.9999962538, round($this->regressionModel->getCorrelation(), 10));
 	}
 }
 ?>
