4614131c22558ca9ebbe49984835a632c4067775
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-09-30 23:02:42 -0400

    Updated unit tests for incomplete regression tests.

--- tests/RegressionModel/ExponentialRegressionTest.php
+++ tests/RegressionModel/ExponentialRegressionTest.php
@@ -6,26 +6,26 @@ class ExponentialRegressionTest extends CustomPHPUnit {
 	private $regressionModel;
 
 	public function __construct() {
-		$datax = array();
-		$datay = array();
+		$datax = array(0, 5, 8, 11, 15, 18, 22, 25, 30, 34, 38, 42, 45, 50);
+		$datay = array(179.5, 168.7, 158.1, 149.2, 141.7, 134.6, 125.4, 123.3, 116.3, 113.2, 109.1, 105.7, 102.2, 100.5);
 
-		//$this->regressionModel = new ExponentialRegression($datax, $datay);
+		$this->regressionModel = new ExponentialRegression($datax, $datay);
 	}
 	
 	public function test_predict() {
-		
+		$this->assertEquals(84.35218941, round($this->regressionModel->predict(60), 8));
 	}
 	
 	public function test_getAlpha() {
-		
+		$this->assertEquals(171.4617283, round($this->regressionModel->getAlpha(), 7));
 	}
 	
 	public function test_getBeta() {
-		
+		$this->assertEquals(0.9882469577, round($this->regressionModel->getBeta(), 10));
 	}
 	
 	public function test_getCorrelation() {
-		
+		$this->assertEquals(-0.9849556976, round($this->regressionModel->getCorrelation(), 10));
 	}
 }
 ?>
--- tests/RegressionModel/LogarithmicRegressionTest.php
+++ tests/RegressionModel/LogarithmicRegressionTest.php
@@ -13,19 +13,19 @@ class LogarithmicRegressionTest extends CustomPHPUnit {
 	}
 	
 	public function test_predict() {
-		
+		//$this->assertEquals(139.329214591, round($this->regressionModel->predict(50), 10));
 	}
 	
 	public function test_getAlpha() {
-		
+		//$this->assertEquals(91.0043962074, round($this->regressionModel->getAlpha(), 10));
 	}
 	
 	public function test_getBeta() {
-		
+		//$this->assertEquals(0.9664963677, round($this->regressionModel->getBeta(), 10));
 	}
 	
 	public function test_getCorrelation() {
-		
+		//$this->assertEquals(0.9341638423, round($this->regressionModel->getCorrelation(), 10));
 	}
 }
 ?>
