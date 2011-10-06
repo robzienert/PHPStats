faaf4f628933607d543b0a178641eac65d192061
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-10-05 21:27:10 -0400

    For real this time: Created unit tests for other discrete distributions.  They still need to be set with proper test values.

---
+++ tests/ProbabilityDistribution/Discrete/PoissonTest.php
@@ -0,0 +1,45 @@
+<?php
+require_once('tests/CustomPHPUnit.php');
+require_once('lib/ProbabilityDistribution/Discrete/Poisson.php');
+
+class PoissonTest extends CustomPHPUnit {
+	private $testObject;
+
+	public function __construct() {
+		$this->testObject = new Poisson(0.5, 5);
+	}
+
+	public function test_rvs() {
+		//$this->assertEquals(, $this->testObject->rvs);
+	}
+
+	public function test_pmf() {
+		$this->assertEquals(, $this->testObject->pmf);
+	}
+
+	public function test_cdf() {
+		$this->assertEquals(, $this->testObject->cdf);
+	}
+
+	public function test_sf() {
+		$this->assertEquals(, $this->testObject->sf);
+	}
+
+	public function test_ppf() {
+		$this->assertEquals(, $this->testObject->ppf);
+	}
+
+	public function test_isf() {
+		$this->assertEquals(, $this->testObject->isf);
+	}
+
+	public function test_stats() {
+		$summaryStats = $this->testObject->stats('mvsk');
+
+		$this->assertEquals(, $summaryStats['mean']);
+		$this->assertEquals(, $summaryStats['variance']);
+		$this->assertEquals(, $summaryStats['skew']);
+		$this->assertEquals(, $summaryStats['kurtosis']);
+	}
+}
+?>
